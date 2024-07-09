55e4e9e9366ee51e7d82e9016e5addfff5ee74f6
Brian Bravol <lumalabravo@gmail.com>
2024-07-09 11:05:35 +0300

    updates

--- readwrite/index.js
+++ readwrite/index.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
 
+//blocing synronous way
 const textIn = fs.readFileSync("input.txt", "utf-8");
 console.log(textIn);
 
 const textOut = `Here is the text: ${textIn} and this is the end of the text.\n Created o ${Date.now()}`;
-console.log(textOut);
 
 fs.writeFileSync("output.txt", textOut); // synchronous
+console.log(textOut);
+//non blocking asyncronous way
+fs.readFile("input.txt", "utf-8", (err, data) => {
+  if (err) throw err;
+  console.log("the data is", data);
+  fs.writeFile("output.txt", data, (err) => {
+    if (err) throw err;
+    console.log("the data is written to file");
+  });
+});
