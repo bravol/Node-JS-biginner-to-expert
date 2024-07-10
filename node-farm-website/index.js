42312892d0c8c309f0b838b7f19047b57ad92e75
Brian Bravol <lumalabravo@gmail.com>
2024-07-10 16:30:01 +0300

    how to export modules

--- node-farm-website/index.js
+++ node-farm-website/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replace_template");
 
 // Read data synchronously at the beginning
 const template_overview = fs.readFileSync(
@@ -17,21 +18,6 @@ const template_product = fs.readFileSync(
 );
 const data = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
-
-//replace template function
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 
 const server = http.createServer((req, res) => {
   // const pathname = req.url;
