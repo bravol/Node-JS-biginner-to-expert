8dbe60f3d55b708c6d61c9e33d0ecbec2ebe313b
Brian Bravol <lumalabravo@gmail.com>
2024-07-17 12:18:57 +0300

    get and post rest API

--- 10-main-project/natours/app.js
+++ 10-main-project/natours/app.js
@@ -2,7 +2,8 @@ const fs = require("fs");
 const express = require("express");
 
 const app = express();
-const port = 3000;
+//middle ware
+app.use(express.json()); //stands btween in the middle of request and response
 
 const data = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -17,6 +18,27 @@ app.get("/api/v1/tours", (req, res) => {
     },
   });
 });
+
+app.post("/api/v1/tours", (req, res) => {
+  //   console.log(req.body);
+  const newId = data[data.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  data.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(data),
+    (err) => {
+      if (err) return res.status(500).send("Error writing file");
+      res.status(201).json({
+        status: "success",
+        data: newTour,
+      });
+    }
+  );
+});
+//port
+const port = 3000;
 app.listen(port, () => {
   console.log("App running on port " + port);
 });
