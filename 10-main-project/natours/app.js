d2b337ea0d3fd8df2b05f0748c608d66606733d4
Brian Bravol <lumalabravo@gmail.com>
2024-07-17 11:45:34 +0300

    now we are crating our own apis using postman

--- 10-main-project/natours/app.js
+++ 10-main-project/natours/app.js
@@ -1,7 +1,22 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 const port = 3000;
+
+const data = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+// routes
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: data.length,
+    data: {
+      tours: data,
+    },
+  });
+});
 app.listen(port, () => {
   console.log("App running on port " + port);
 });
