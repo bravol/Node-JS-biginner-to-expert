cbeb5fb5c7ce42e22fe6c4380cbed783dd0e9c0d
Brian Bravol <lumalabravo@gmail.com>
2024-07-19 14:33:12 +0300

    condiguring eslint, and other updates made

--- 10-main-project/natours/controllers/tourController.js
+++ 10-main-project/natours/controllers/tourController.js
@@ -58,11 +58,8 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  // console.log(req.body);
-
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
-
   tours.push(newTour);
 
   fs.writeFile(
--- 10-main-project/natours/server.js
+++ 10-main-project/natours/server.js
@@ -1,6 +1,9 @@
+const dotenv = require("dotenv");
 const app = require("./app");
-//port
+
+dotenv.config({ path: "./config.env" });
+
 const port = 3000;
 app.listen(port, () => {
-  console.log("App running on port " + port);
+  console.log(`App running on port ${port}...`);
 });
