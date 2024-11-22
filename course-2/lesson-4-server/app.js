9d4fb080a8d096d7b24c48fc681247dd2a519110
Brian Bravol <lumalabravo@gmail.com>
2024-11-22 16:35:29 +0300

    parsing data

--- course-2/lesson-4-server/app.js
+++ course-2/lesson-4-server/app.js
@@ -1,18 +1,32 @@
 const http = require("http");
 
+const product = {
+  pcode: 1001,
+  product: "Apple",
+};
+
 const server = http.createServer((req, res) => {
-  // //   console.log(req.headers);
-  // //   console.log(req.url);
+  //   console.log(req.headers);
+  //   console.log(req.url);
   //   console.log(req.method);
   //   console.log("server is running...");
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html><head><title>Node Server</title></head>");
-  res.write("<body><h1>Server running...</h1>");
-  res.write("<h3>Hi</h3>");
-  res.write("</body></html>");
-  console.log("server is running...");
-
-  res.end();
+  let url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html><head><title>Node Server</title></head>");
+    res.write("<body><h1>Home Page</h1>");
+    res.write("<h3>" + req.url + "</h3>");
+    res.write("</body></html>");
+    return res.end();
+  } else if (url === "/data") {
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(product));
+    return res.end();
+  } else if (url === "/getdate") {
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify({ now: new Date() }));
+    return res.end();
+  }
 });
 
 server.listen(3000);
